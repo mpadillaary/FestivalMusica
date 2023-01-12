b8d49cf7803793d0443b095e1aa3b9de188166ff
Miguel Padilla <miguel.padilla@elkayinteriorsystems.com>
2023-01-12 10:39:20 -0600

    FestivalMusicaV1

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ const plumber = require("gulp-plumber");
 const autoprefixer = require("autoprefixer");
 const cssnano = require("cssnano");
 const postcss = require("gulp-postcss");
+const sourcemaps = require("gulp-sourcemaps");
 
 
 
@@ -15,11 +16,16 @@ const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
 const avif = require("gulp-avif");
 
+//** JavaScript */
+const terser = require("gulp-terser-js");
+
 function css(done) {
   src("src/scss/**/*.scss") // Identificar el archivo de SASS
+    .pipe(sourcemaps.init())
     .pipe(plumber())
     .pipe(sass()) // Compilar
     .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(sourcemaps.write('.'))
     .pipe(dest("build/css")); // Almacenarla en el disco duro
 
   done(); //**Callback que avisa a gulp cuando llegamos al final */
@@ -63,6 +69,9 @@ function versionAvif(done) {
 
 function javascript(done) {
   src('src/js/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(terser())
+    .pipe(sourcemaps.write('.'))
     .pipe(dest('build/js'));
 
   done();
